ef720e17cf3a5159cc97f18a5a060e42dc2fd01e
Mluleki Amampondo Mkhunyana <120003479+Amampondo@users.noreply.github.com>
2025-07-19 22:18:21 +0200

    Add files via upload

--- profile/index.js
+++ profile/index.js
@@ -1,3 +1,25 @@
 function toAbout(){
     window.location.assign("about.html");
 }
+function toWorkExperience(){
+    window.location.assign("workexperience.html");
+}
+function toProjects(){
+    window.location.assign("projects.html");
+}
+document.addEventListener('mousemove',function(e){
+    let circle = document.getElementById("circle");
+    let delta = circle.offsetWidth/2;
+    circle.style.left = e.pageX - delta - 20 + 'px';
+    circle.style.top = e.pageY - delta - 20 + 'px';
+})
+function enlarge(){
+    let circle = document.getElementById("circle");
+    let innerCircle = document.getElementById("innerCircle");
+    circle.style.opacity = 0;
+    innerCircle.style.opacity = 1;
+}
+function contract(){
+    let circle = document.getElementById("circle");
+    circle.style.opacity = 1;
+}
